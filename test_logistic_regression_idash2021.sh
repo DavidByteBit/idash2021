a5c72f9c59ec45064a6b1adbad2a9ca56cecca82
Ricardo José Menezes Maia <ricardo.jmm@gmail.com>
2021-08-10 13:01:29 -0300

    idash2021

--- test_logistic_regression_idash2021.sh
+++ test_logistic_regression_idash2021.sh
@@ -2,18 +2,18 @@
 
 echo "experiments idash2021" > results.txt
 
-for i in $(seq 1 30)
+n_examples_train=700
+n_examples_test=131
+n_features=1874
+n_epochs=10
+batch_size=128
+
+for i in $(seq 1 1)
 do
-    n_examples_train=700
-    n_examples_test=131
-    n_features=1874
-    n_epochs=200
-    batch_size=128
-    param_report_loss=True
-
-    ./compile.py -R 64 logistic_regression_idash2021 $n_examples_train $n_examples_test $n_features $n_epochs $batch_size $param_report_loss >> results.txt
-
-    Scripts/ring.sh logistic_regression_idash2021-$n_examples_train-$n_examples_test-$n_features-$n_epochs-$batch_size-$param_report_loss >> results.txt
+#    ./compile.py -R 64 logistic_regression_idash2021 $n_examples_train $n_examples_test $n_features $n_epochs $batch_size >> results.txt
+#    Scripts/ring.sh logistic_regression_idash2021-$n_examples_train-$n_examples_test-$n_features-$n_epochs-$batch_size >> results.txt
+    ./compile.py -R 64 logistic_regression_idash2021 $n_examples_train $n_examples_test $n_features $n_epochs $batch_size
+    Scripts/ring.sh logistic_regression_idash2021-$n_examples_train-$n_examples_test-$n_features-$n_epochs-$batch_size
 done
 
 cat results.txt | grep accuracy
