dc7a6dbfda9ceb084cdd5c7302d1344f05cb7861
Ricardo José Menezes Maia <ricardo.jmm@gmail.com>
2021-08-10 13:39:48 -0300

    idash2021

--- test_logistic_regression_idash2021.sh
+++ test_logistic_regression_idash2021.sh
@@ -1,19 +1,33 @@
 #!/bin/bash
 
-echo "experiments idash2021" > results.txt
+timestamp=$(date +%d%m%Y%H%M%S)
+results="results$timestamp.txt"
+echo "Results in $results"
+echo "experiments idash2021" > $results
 
 n_examples_train=700
 n_examples_test=131
 n_features=1874
 n_epochs=10
 batch_size=128
+n_times=5
 
-for i in $(seq 1 1)
+for i in $(seq 1 $n_times)
 do
-#    ./compile.py -R 64 logistic_regression_idash2021 $n_examples_train $n_examples_test $n_features $n_epochs $batch_size >> results.txt
-#    Scripts/ring.sh logistic_regression_idash2021-$n_examples_train-$n_examples_test-$n_features-$n_epochs-$batch_size >> results.txt
-    ./compile.py -R 64 logistic_regression_idash2021 $n_examples_train $n_examples_test $n_features $n_epochs $batch_size
-    Scripts/ring.sh logistic_regression_idash2021-$n_examples_train-$n_examples_test-$n_features-$n_epochs-$batch_size
+    ./compile.py -R 64 logistic_regression_idash2021 $n_examples_train $n_examples_test $n_features $n_epochs $batch_size >> $results
+    Scripts/ring.sh logistic_regression_idash2021-$n_examples_train-$n_examples_test-$n_features-$n_epochs-$batch_size >> $results
+#    ./compile.py -R 64 logistic_regression_idash2021 $n_examples_train $n_examples_test $n_features $n_epochs $batch_size
+#    Scripts/ring.sh logistic_regression_idash2021-$n_examples_train-$n_examples_test-$n_features-$n_epochs-$batch_size
 done
 
-cat results.txt | grep accuracy
+
+
+
+echo "Accuracy $timestamp - $results" >> accuracy.txt
+cat $results | grep accuracy >> accuracy.txt
+echo "Time $timestamp - $results" >> times.txt
+cat $results | grep Time10 >> times.txt
+
+cat accuracy.txt
+
+cat times.txt
